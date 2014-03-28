8db1661d0c0779ade7adc557985691b3d645aa00
Collin Petit <collin-petit@uiowa.edu>
2014-03-28 14:01:34 -0500

    Completed Guess Number App

---
+++ programming7-cpetit/GuessNumber/GuessNumber/GuessNumber.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GuessNumberForm
+   Inherits System.Windows.Forms.Form
+
+   'Form overrides dispose to clean up the component list.
+   <System.Diagnostics.DebuggerNonUserCode()> _
+   Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+      Try
+         If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+         End If
+      Finally
+         MyBase.Dispose(disposing)
+      End Try
+   End Sub
+
+   'Required by the Windows Form Designer
+   Private components As System.ComponentModel.IContainer
+
+   'NOTE: The following procedure is required by the Windows Form Designer
+   'It can be modified using the Windows Form Designer.  
+   'Do not modify it using the code editor.
+   <System.Diagnostics.DebuggerStepThrough()> _
+   Private Sub InitializeComponent()
+        Me.questionLabel = New System.Windows.Forms.Label()
+        Me.guessLabel = New System.Windows.Forms.Label()
+        Me.resultLabel = New System.Windows.Forms.Label()
+        Me.enterButton = New System.Windows.Forms.Button()
+        Me.newGameButton = New System.Windows.Forms.Button()
+        Me.guessTextBox = New System.Windows.Forms.TextBox()
+        Me.outputLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'questionLabel
+        '
+        Me.questionLabel.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.questionLabel.Location = New System.Drawing.Point(20, 14)
+        Me.questionLabel.Name = "questionLabel"
+        Me.questionLabel.Size = New System.Drawing.Size(224, 41)
+        Me.questionLabel.TabIndex = 3
+        Me.questionLabel.Text = "I have a number between 1 and 100. Can you guess my number?"
+        Me.questionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'guessLabel
+        '
+        Me.guessLabel.AutoSize = True
+        Me.guessLabel.Location = New System.Drawing.Point(20, 82)
+        Me.guessLabel.Name = "guessLabel"
+        Me.guessLabel.Size = New System.Drawing.Size(41, 15)
+        Me.guessLabel.TabIndex = 4
+        Me.guessLabel.Text = "Guess:"
+        '
+        'resultLabel
+        '
+        Me.resultLabel.AutoSize = True
+        Me.resultLabel.Location = New System.Drawing.Point(20, 122)
+        Me.resultLabel.Name = "resultLabel"
+        Me.resultLabel.Size = New System.Drawing.Size(42, 15)
+        Me.resultLabel.TabIndex = 5
+        Me.resultLabel.Text = "Result:"
+        '
+        'enterButton
+        '
+        Me.enterButton.Location = New System.Drawing.Point(169, 79)
+        Me.enterButton.Name = "enterButton"
+        Me.enterButton.Size = New System.Drawing.Size(75, 23)
+        Me.enterButton.TabIndex = 1
+        Me.enterButton.Text = "Enter"
+        Me.enterButton.UseVisualStyleBackColor = True
+        '
+        'newGameButton
+        '
+        Me.newGameButton.Enabled = False
+        Me.newGameButton.Location = New System.Drawing.Point(169, 118)
+        Me.newGameButton.Name = "newGameButton"
+        Me.newGameButton.Size = New System.Drawing.Size(75, 23)
+        Me.newGameButton.TabIndex = 2
+        Me.newGameButton.Text = "New Game"
+        Me.newGameButton.UseVisualStyleBackColor = True
+        '
+        'guessTextBox
+        '
+        Me.guessTextBox.Location = New System.Drawing.Point(68, 79)
+        Me.guessTextBox.Name = "guessTextBox"
+        Me.guessTextBox.Size = New System.Drawing.Size(87, 23)
+        Me.guessTextBox.TabIndex = 0
+        Me.guessTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'outputLabel
+        '
+        Me.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.outputLabel.Location = New System.Drawing.Point(68, 118)
+        Me.outputLabel.Name = "outputLabel"
+        Me.outputLabel.Size = New System.Drawing.Size(87, 23)
+        Me.outputLabel.TabIndex = 6
+        Me.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GuessNumberForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(266, 152)
+        Me.Controls.Add(Me.outputLabel)
+        Me.Controls.Add(Me.guessTextBox)
+        Me.Controls.Add(Me.newGameButton)
+        Me.Controls.Add(Me.enterButton)
+        Me.Controls.Add(Me.resultLabel)
+        Me.Controls.Add(Me.guessLabel)
+        Me.Controls.Add(Me.questionLabel)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "GuessNumberForm"
+        Me.Text = "Guess the Number"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+   Friend WithEvents questionLabel As System.Windows.Forms.Label
+   Friend WithEvents guessLabel As System.Windows.Forms.Label
+   Friend WithEvents resultLabel As System.Windows.Forms.Label
+   Friend WithEvents enterButton As System.Windows.Forms.Button
+   Friend WithEvents newGameButton As System.Windows.Forms.Button
+   Friend WithEvents guessTextBox As System.Windows.Forms.TextBox
+   Friend WithEvents outputLabel As System.Windows.Forms.Label
+
+End Class
---
+++ programming7-cpetit/GuessNumber/GuessNumber/GuessNumber.vb
@@ -0,0 +1,70 @@
+﻿Public Class GuessNumberForm
+
+    ' creat a randomObject
+    'Dim randomObject As Random = New Random
+
+    Dim randomObject As New Random
+
+    ' generate a random number
+
+    Dim randomNumber As Integer = randomObject.Next(1, 101) ' returns a number between 1 - 100
+
+    Private Sub enterButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles enterButton.Click
+
+        ' get user input
+        Dim guessNumber As Integer = Convert.ToInt32(guessTextBox.Text)
+
+        ' check number
+        If guessNumber = randomNumber Then
+            outputLabel.Text = "Correct!"
+            newGameButton.Enabled = True
+            enterButton.Enabled = False
+        ElseIf guessNumber > randomNumber Then
+            outputLabel.Text = "Too high..."
+        ElseIf guessNumber < randomNumber Then
+            outputLabel.Text = "Too low..."
+        End If
+
+        ' focus the TextBox
+
+        guessTextBox.Focus()
+
+    End Sub ' enterButton_Click
+
+    Private Sub newGameButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles newGameButton.Click
+        ' start a new game
+
+        randomNumber = randomObject.Next(1, 101)
+        enterButton.Enabled = True
+
+        newGameButton.Enabled = False
+
+        guessTextBox.Clear()
+        outputLabel.Text = String.Empty
+        guessTextBox.Focus()
+
+
+
+    End Sub ' newGameButton_Click
+
+    Private Sub guessTextBox_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles guessTextBox.TextChanged
+        ' Clear output label
+        outputLabel.Text = ""
+
+    End Sub 'guessTextBox_TextChanged
+End Class ' GuessNumberForm
+
+' **************************************************************************
+' * (C) Copyright 1992-2013 by Deitel & Associates, Inc. and               *
+' * Pearson Education, Inc. All Rights Reserved.                           *
+' *                                                                        *
+' * DISCLAIMER: The authors and publisher of this book have used their     *
+' * best efforts in preparing the book. These efforts include the          *
+' * development, research, and testing of the theories and programs        *
+' * to determine their effectiveness. The authors and publisher make       *
+' * no warranty of any kind, expressed or implied, with regard to these    *
+' * programs or to the documentation contained in these books. The authors *
+' * and publisher shall not be liable in any event for incidental or       *
+' * consequential damages in connection with, or arising out of, the       *
+' * furnishing, performance, or use of these programs.                     *
+' **************************************************************************
